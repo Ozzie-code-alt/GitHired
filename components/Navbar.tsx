a01ca63e902e6dcc55ee497acdf3ee1ef5b9eabb
Justin Santos <83016833+Ozzie-code-alt@users.noreply.github.com>
2024-06-15 14:55:55 +0800

    feature: added Navbar component

---
+++ components/Navbar.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function Navbar() {
+  return (
+    <div className='  py-10  gap-8 w-full flex flex-col  lg:hidden px-10 '>
+      <div className='flex w-full justify-between '>
+        <div>Justin-Santos.com</div>
+        <div>Some Icons Here</div>
+      </div>
+      <div className='border  ' />
+    </div>
+  );
+}
+
+export default Navbar;
