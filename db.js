b30643d26b187dd7f62922a2eaa4b047b9760fbc
Justin Santos <83016833+Ozzie-code-alt@users.noreply.github.com>
2024-06-18 16:14:28 +0800

    Create db.js

---
+++ db.js
@@ -0,0 +1,31 @@
+{
+    "posts": [
+        {
+            "id": 1,
+            "title": "Justin Post"
+        },
+        {
+            "id": 2,
+            "title": "Justin Post 2"
+        },
+        {
+            "id": 3,
+            "title": "Justin Post  3"
+        }
+    ],
+    "comments": [
+        {
+            "id": 1,
+            "body": "some comment Justin",
+            "postId": 1
+        },
+        {
+            "id": 2,
+            "body": "some comment Justin3",
+            "postId": 1
+        }
+    ],
+    "profile": {
+        "name": "typicode"
+    }
+}
