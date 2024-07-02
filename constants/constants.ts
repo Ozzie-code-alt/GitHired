be22ab03d036f78f51358d14e51c7187852ea763
Justin Santos <83016833+Ozzie-code-alt@users.noreply.github.com>
2024-07-02 18:34:17 +0800

    feat:add Wordpress Projects and filter

--- constants/constants.ts
+++ constants/constants.ts
@@ -1,143 +1,149 @@
+import { title } from 'process';
 
 export const LEFTNAV_CONSTANTS = [
   {
     title: 'Justin',
-    value: "1",
+    value: '1',
     bgColor: 'bg-[rgba(255,0,247,.3)]',
     borderColor: 'border-[rgba(255,0,246,.8)]',
     textColor: 'text-[#680045]',
     px: 'px-10',
-    py: 'py-2',
-   
+    py: 'py-2'
   },
   {
     title: 'Menu',
-    value: "2",
+    value: '2',
     bgColor: 'bg-[rgba(0,208,255,.1)]',
     borderColor: 'border-[#00eaff]',
     textColor: 'text-[#005f68]',
     px: 'px-10',
-    py: 'py-2',
-    
+    py: 'py-2'
   },
   {
     title: 'Projects',
-    value: "3",
+    value: '3',
     bgColor: 'bg-[rgba(0,255,17,.1)]',
     borderColor: 'border-[#0f1]',
     textColor: 'text-[#005205]',
     px: 'px-10',
-    py: 'py-2',
+    py: 'py-2'
   }
 ];
 
-
 export const CHECKBOXES_CONSTANTS = [
   {
-    title: "Next JS",
-    value: "1" ,
+    title: 'Next JS',
+    value: '1'
   },
   {
-    title: "Astro",
-    value: "2" ,
+    title: 'Astro',
+    value: '2'
   },
   {
-    title: "Prisma",
-    value: "3" ,
+    title: 'Prisma',
+    value: '3'
   },
   {
-    title: "MongoDB",
-    value: "4" ,
+    title: 'MongoDB',
+    value: '4'
   },
   {
-    title: "React",
-    value: "5" ,
+    title: 'React',
+    value: '5'
+  },
+  {
+    title: 'Tailwind',
+    value: '6'
+  },
+  {
+    title: 'Framer',
+    value: '7'
+  },
+  {
+    title: 'Wordpress',
+    value: '1'
+  },
+  {
+    title: 'Elementor',
+    value: '2'
   }
-  ,
-  {
-    title: "Tailwind",
-    value: "6" ,
-  },
-  {
-    title: "Framer",
-    value: "7" ,
-  }
-
-
-]
+];
 
 export const FOOTER_CONSTANTS = [
   {
-    title: "About"
+    title: 'About'
   },
   {
-    title: "Contact"
+    title: 'Contact'
   },
   {
-    title: "PDF"
+    title: 'PDF'
   }
-]
-
+];
 
 export const PROJECTS_CONSTANTS = [
   {
-    title: "JCI Regatta Origanization",
-    img: "/Projects/JCI.webp",
-    tags: ["Astro", "Tailwind"], 
-    value:"2",
-    route: "https://jci-regatta-iloilo-astro.vercel.app/",
-    transition: "Effect2"
+    title: 'JCI Regatta Origanization',
+    img: '/Projects/JCI.webp',
+    tags: ['Astro', 'Tailwind'],
+    value: '2',
+    route: 'https://jci-regatta-iloilo-astro.vercel.app/',
+    transition: 'Effect2'
   },
   {
-    title: "Apple Iphone 15 Web Clone ",
-    img: "/Projects/AppleWebclone.webp",
-    tags: ["React","GSAP", "Tailwind"], 
-    value:"3",
-    route: "https://apple-webclone-beta.vercel.app/",
-    transition: "red-box"
+    title: 'Apple Iphone 15 Web Clone ',
+    img: '/Projects/AppleWebclone.webp',
+    tags: ['React', 'GSAP', 'Tailwind'],
+    value: '3',
+    route: 'https://apple-webclone-beta.vercel.app/',
+    transition: 'red-box'
   },
   {
-    title: "Putok Truno Camp",
-    img: "/Projects/PutokCamp.webp",
-    tags: ["React","Tailwind"], 
-    value:"4",
-    route: "https://starlight-six.vercel.app/",
-    transition: "red-box"
-  }
-  ,
+    title: 'PurrfectCompanions',
+    img: '/Projects/Purrfect.webp',
+    tags: ['Wordpress', 'Elementor'],
+    value: '1',
+    route: 'https://hackathon-website-gilt.vercel.app/',
+    transition: 'red-box'
+  },
   {
-    title: "Wedding - Promises",
-    img: "/Projects/Promises.webp",
-    tags: ["NextJs", "Framer", "MongoDB"], 
-    value:"3",
-    route: "https://promises-3.vercel.app/",
-    transition: "red-box"
-  }
-  ,
+    title: 'Putok Truno Camp',
+    img: '/Projects/PutokCamp.webp',
+    tags: ['React', 'Tailwind'],
+    value: '4',
+    route: 'https://starlight-six.vercel.app/',
+    transition: 'red-box'
+  },
   {
-    title: "HooBank - Landing Page",
-    img: "/Projects/BankApp.webp",
-    tags: ["NextJs", "Framer", "Tailwind"], 
-    value:"2",
-    route: "https://blue-horizon-xi.vercel.app/",
-    transition: "red-box"
-  }
-  ,
+    title: 'Wedding - Promises',
+    img: '/Projects/Promises.webp',
+    tags: ['NextJs', 'Framer', 'MongoDB'],
+    value: '3',
+    route: 'https://promises-3.vercel.app/',
+    transition: 'red-box'
+  },
   {
-    title: "Lakbay",
-    img: "/Projects/Lakbay.webp",
-    tags: ["React", "CSS"], 
-    value:"2",
-    route: "/project4",
-    transition: "red-box"
-  }
-  ,
+    title: 'HooBank - Landing Page',
+    img: '/Projects/BankApp.webp',
+    tags: ['NextJs', 'Framer', 'Tailwind'],
+    value: '2',
+    route: 'https://blue-horizon-xi.vercel.app/',
+    transition: 'red-box'
+  },
   {
-    title: "Open Bible",
-    img: "/Projects/openBible.webp",
-    tags: ["HTML", "CSS", "JS"], 
-    value:"3",
-    route: "https://hackathon-website-gilt.vercel.app/",
-    transition: "red-box"
+    title: 'Lakbay',
+    img: '/Projects/Lakbay.webp',
+    tags: ['React', 'CSS'],
+    value: '2',
+    route: '/project4',
+    transition: 'red-box'
+  },
+  {
+    title: 'Open Bible',
+    img: '/Projects/openBible.webp',
+    tags: ['HTML', 'CSS', 'JS'],
+    value: '3',
+    route: 'https://hackathon-website-gilt.vercel.app/',
+    transition: 'red-box'
   }
-]
+];
