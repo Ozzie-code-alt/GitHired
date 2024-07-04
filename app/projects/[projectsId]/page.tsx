a52c681c4dff8c9f2665dd9c42790e5b6ae23e36
Justin Santos <83016833+Ozzie-code-alt@users.noreply.github.com>
2024-07-05 00:17:29 +0800

    feat:project page parralax

--- app/projects/[projectsId]/page.tsx
+++ app/projects/[projectsId]/page.tsx
@@ -1,15 +1,11 @@
 import { ProjectData } from '@/constants/constants';
-
+import { ProjectParallaxContent } from '@/components/ProjectContent';
 export default function Projects({ params }: { params: { projectsId: string } }) {
   const { projectsId } = params;
   const project = ProjectData.find((value) => value.id === projectsId);
   return (
     <>
-      {project && (
-        <div>
-          <p>this is project Data {JSON.stringify(project.title)}</p>
-        </div>
-      )}
+      <ProjectParallaxContent projectData={project} />
     </>
   );
 }
