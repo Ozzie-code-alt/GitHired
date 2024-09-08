0956bf25ee771b588cdee8914c9e8fe9b0ba9cf6
Justin Santos <83016833+Ozzie-code-alt@users.noreply.github.com>
2024-09-08 22:25:29 +0800

    feat: implement ipad cursor package

--- app/page.tsx
+++ app/page.tsx
@@ -1,24 +1,28 @@
 'use client';
 import Navbar from '@/components/Navbar';
 import dynamic from 'next/dynamic';
+import { IPadCursorProvider, useIPadCursor } from 'ipad-cursor/react';
+import type { IpadCursorConfig } from 'ipad-cursor';
 export default function Home() {
-
   const ContentDynamic = dynamic(() => import('../components/Content'), { ssr: false });
-
+  const config: IpadCursorConfig = {};
+  useIPadCursor();
 
   return (
-    <main className='flex dark:bg-black  relative justify-center  '>
-      <div className=' w-full relative  '>
-        {/* <div className={` ${timer === true ? 'lg:flex' : 'hidden'}`}>
+    <IPadCursorProvider config={config}>
+      <main className='flex dark:bg-black  relative justify-center  '>
+        <div className=' w-full relative  '>
+          {/* <div className={` ${timer === true ? 'lg:flex' : 'hidden'}`}>
           <Navbar />
           <ContentDynamic />
         </div> */}
 
-        <div className={` 'lg:flex' : 'hidden'}`}>
-          <Navbar />
-          <ContentDynamic />
+          <div className={` 'lg:flex' : 'hidden'}`}>
+            <Navbar />
+            <ContentDynamic />
+          </div>
         </div>
-      </div>
-    </main>
+      </main>
+    </IPadCursorProvider>
   );
 }
--- components/Checkboxes.tsx
+++ components/Checkboxes.tsx
@@ -2,12 +2,25 @@
 
 import { CHECKBOXES_CONSTANTS } from '@/constants/constants';
 import React, { useState } from 'react';
+import { IPadCursorProvider, useIPadCursor } from 'ipad-cursor/react';
+import type { IpadCursorConfig } from 'ipad-cursor';
 
 interface CheckboxesProps {
   upData: (data: string[]) => void;
 }
 
 const Checkboxes: React.FC<CheckboxesProps> = ({ upData }) => {
+  const config: IpadCursorConfig = {};
+  const { customCursorStyle } = useIPadCursor();
+
+  const style = customCursorStyle
+    ? customCursorStyle({
+        backdropBlur: 0,
+        durationBackdropFilter: 1000,
+        radius: 9999
+      })
+    : '';
+
   const [value, grabValue] = useState<string[]>([]);
 
   const handleCheckboxChange = (title: string) => {
@@ -21,12 +34,15 @@ const Checkboxes: React.FC<CheckboxesProps> = ({ upData }) => {
     upData(newValue);
   };
   return (
-    <div className='w-full '>
-      <div className='flex  gap-2  max-w-[700px] lg:max-w-[400px]  flex-wrap '>
-        {CHECKBOXES_CONSTANTS.map((item, index) => (
-          <div
-            key={index}
-            className={`w-fit px-2 
+    <IPadCursorProvider config={config}>
+      <div className='w-full '>
+        <div className='flex  gap-2  max-w-[700px] lg:max-w-[400px]  flex-wrap '>
+          {CHECKBOXES_CONSTANTS.map((item, index) => (
+            <div
+              data-cursor='block'
+              data-cursor-style={style}
+              key={index}
+              className={`w-fit px-2 
           border gap-2 rounded-full  font-checkboxes flex justify-center items-center
             ${
               item.value === '1'
@@ -46,24 +62,25 @@ const Checkboxes: React.FC<CheckboxesProps> = ({ upData }) => {
                             : ''
             } 
           `}
-          >
-            <input
-              type='checkbox'
-              id={item.title}
-              name={item.title}
-              key={index}
-              checked={value.includes(item.title)}
-              onChange={() => {
-                handleCheckboxChange(item.title);
-              }}
-            />
-            <label className='text-gray-500' htmlFor={item.title}>
-              {item.title}
-            </label>
-          </div>
-        ))}
+            >
+              <input
+                type='checkbox'
+                id={item.title}
+                name={item.title}
+                key={index}
+                checked={value.includes(item.title)}
+                onChange={() => {
+                  handleCheckboxChange(item.title);
+                }}
+              />
+              <label className='text-gray-500' htmlFor={item.title}>
+                {item.title}
+              </label>
+            </div>
+          ))}
+        </div>
       </div>
-    </div>
+    </IPadCursorProvider>
   );
 };
 
